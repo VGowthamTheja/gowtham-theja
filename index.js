2943e98be0d88346f8001eea3634d50540051ac0
Vempalli Gowtham Theja <vempalligowthamtheja@gmail.com>
2023-10-26 18:59:21 +0530

    first module publish

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+console.log(`
+Vempalli Gowtham Theja
+Github: https://github.com/VGowthamTheja
+NPM: https://www.npmjs.com/~gowthamtheja
+GMail: vempalligowthamtheja@gmail.com
+`)
+
+module.exports = {
+    name: 'Gowtham Theja',
+    sayHello: function() {
+        console.log('Hello, from GowthamTheja!')
+    }
+}
